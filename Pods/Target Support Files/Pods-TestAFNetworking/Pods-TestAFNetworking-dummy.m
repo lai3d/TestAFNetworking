d10e87122d5a6be446ab62b87a699009c2fc6cb8
lai3d <laiyanlin@gmail.com>
2016-05-11 11:30:31 +0800

    pod update;

---
+++ Pods/Target Support Files/Pods-TestAFNetworking/Pods-TestAFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestAFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_TestAFNetworking
+@end
--- TestAFNetworking/AppDelegate.m
+++ TestAFNetworking/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AFNetworkActivityIndicatorManager.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:4 * 1024 * 1024 diskCapacity:20 * 1024 * 1024 diskPath:nil];
+    [NSURLCache setSharedURLCache:URLCache];
+    
+    [[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
     return YES;
 }
 
