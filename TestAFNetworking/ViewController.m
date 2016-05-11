c06ac23407d25b9e50d61122cb592992999ba900
lai3d <laiyanlin@gmail.com>
2016-05-11 18:16:15 +0800

    Two buttons to trigger two html requests.

--- TestAFNetworking/ViewController.m
+++ TestAFNetworking/ViewController.m
@@ -14,7 +14,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *label;
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 
-@property (strong, nonatomic) NSURLRequest *htmlRequest;
+@property (strong, nonatomic) NSURLRequest *homeHtmlRequest;
+@property (strong, nonatomic) NSURLRequest *casinotmlRequest;
 
 @end
 
@@ -24,7 +25,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.htmlRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://global.138.com/en-gb/mobile/casino#!/slots"]];
+    self.homeHtmlRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://mobsbglobal.138.com"]];
+    
+    self.casinotmlRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://global.138.com/en-gb/mobile/casino#!/slots"]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,8 +35,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)loadWebView {
-    [self.webView loadRequest:self.htmlRequest
+- (void)loadWebView:(NSURLRequest *)htmlRequest {
+    [self.webView loadRequest:htmlRequest
                      progress:nil
                       success:^NSString * _Nonnull(NSHTTPURLResponse * _Nonnull response, NSString * _Nonnull HTML) {
                           NSLog(@"Success");
@@ -42,9 +45,15 @@
                       failure:nil];
 }
 
-- (IBAction)touchUpInside:(id)sender {
-    NSLog(@"Touch up inside");
+- (IBAction)homeClick:(id)sender {
+    NSLog(@"Home Clicked");
     
-    [self loadWebView];
+    [self loadWebView:self.homeHtmlRequest];
+}
+
+- (IBAction)casinoClick:(id)sender {
+    NSLog(@"Casino Clicked");
+    
+    [self loadWebView:self.casinotmlRequest];
 }
 @end
